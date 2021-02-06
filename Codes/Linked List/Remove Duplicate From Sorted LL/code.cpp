d30b49f7b5a56ac6439bea63bf3b64c185063354
Chitransh Saxena <chitransh.aang@gmail.com>
2021-02-07 01:20:34 +0530

    Remove Duplicate from LL

---
+++ Codes/Linked List/Remove Duplicate From Sorted LL/code.cpp
@@ -0,0 +1,21 @@
+Node *removeDuplicates(Node *root)
+{
+    
+    if(root == NULL) {			// Base Corner Case
+        return NULL;
+    }
+    Node* curr = root;
+    // Node* prev = NULL, *next = NULL;
+    
+    while(curr && curr->next) {
+        
+        if(curr->data == curr->next->data) {		// Comparing current and next element
+            curr->next = curr->next->next;			// Making current's next point to one element ahead than it usually does
+        }
+        else {
+            curr = curr->next;
+        }
+    }
+    return root;
+ 
+}
