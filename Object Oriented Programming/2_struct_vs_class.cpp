1cf6e793a2d57acbc4b2486e442c269f11c04382
Chitransh Saxena <chitransh.aang@gmail.com>
2021-05-17 11:23:37 +0530

    Class vs Struct

---
+++ Object Oriented Programming/2_struct_vs_class.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// Struct vs class
+// Almost no difference, apart from visibility
+/*
+    Class - If we do not specifiy any access modifier, a class is private by default
+    Struct - Everything is public by default. It exists in C++ to maintain backwards compatibility with C.
+
+    So, if we wanna talk about usage, we can use struct when it comes to Plain Old Data (POD). So, if we want a data type that has different data types in it
+    Like, linked list node, or something like that, we can use struct
+
+    Classes are more of like a well defined big package of different data types and methods to get or set or manipulate those variables.
+
+    Btw, inheritence works in both classes and structures, but C struct is different from C++ struct. 
+
+*/
+
+int main ()
+{
+
+
+}
